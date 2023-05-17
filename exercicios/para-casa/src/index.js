7a3dd870766460aaa1f2613debcf8c9f783bc049
AylaDelavi <ayladelavi@gmail.com>
2023-05-17 14:15:17 -0300

    tarefinha semana 8

---
+++ exercicios/para-casa/src/index.js
@@ -0,0 +1,60 @@
+const express = require("express") //chama o express
+const ghibliFilmesJson = require("./model/ghiblifilmes.json")
+const app = express()
+
+app.use(express.json())
+
+app.listen(7777, () => {
+    console.log("O servidor está na porta 7777 :)")
+})
+
+app.get("/", (request, response) => {
+    response.status(200).json([{
+        "message": "deu certoo, está roteando"
+    }])
+})
+
+app.get("/filmes", (request, response) => {
+    response.status(200).send(ghibliFilmesJson)
+})
+
+app.get("/filmes/:id", (request, response) => {
+    let idRequest = request.params.id
+    let filmeEcontrado = ghibliFilmesJson.find(filme => filme.id == idRequest)
+    response.status(200).send(filmeEcontrado)
+})
+
+app.get("/director", (request, response) => {
+    let directorRequest = request.query.director.toLocaleLowerCase()
+    console.log(directorRequest)
+    let filmeEcontrado = ghibliFilmesJson.filter(filme => filme.director.toLocaleLowerCase().includes(directorRequest))
+    response.status(200).send(filmeEcontrado)
+})
+
+app.post("/filmes", (request, response) => {  
+    let titleRequest = request.body.title 
+    let original_titleRequest = request.body.original_title 
+    let original_title_romanisedRequest = request.body.original_title_romanised
+    let descriptionRequest = request.body.description
+    let directorRequest = request.body.director
+    let producerRequest = request.body.producer
+    let release_dateRequest = request.body.release_date
+    let running_timeRequest = request.body.running_time
+
+    let novoFilme = {
+        id: (ghibliFilmesJson.length) + 1,
+        title: titleRequest,
+        original_title: original_titleRequest,
+        original_title_romanised: original_title_romanisedRequest,
+        description: descriptionRequest,
+        director: directorRequest,
+        producer: producerRequest,
+        release_date: release_dateRequest,
+        running_time: running_timeRequest
+    }
+    ghibliFilmesJson.push(novoFilme)
+    response.status(201).json([{
+        "message": "Seu filmes foi cadastrado com sucesso :)!", 
+        novoFilme
+    }])
+})
