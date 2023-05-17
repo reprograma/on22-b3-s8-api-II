2b45272be24d33798a1df9f0f55baea557d3e891
Jamile <milli.santana2017@gmail.com>
2023-05-17 13:26:49 -0300

    feat:Adiciona rota para buscar um filme pelo titulo

--- exercicios/para-casa/src/index.js
+++ exercicios/para-casa/src/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const ghibliFilmesJson = require("./model/ghiblifilmes.json");
+const ghibliFilmsJson = require("./model/ghiblifilmes.json");
 const app = express();
 
 app.use(express.json());
@@ -7,7 +7,14 @@ app.listen(3030, ()=> {
     console.log("Servidor rodando na porta 3030");
 });
 
-// criando uma rota principal
+// criando uma rota que traz todos os filmes
 app.get("/filmesGhibli",(_req, res)=>{
-    res.status(200).send(ghibliFilmesJson);
+    res.status(200).send(ghibliFilmsJson);
+});
+
+// Rota para pegar um filme pelo título
+app.get("/filmesGhibli/title", (req, res)=>{
+    let titleRequest = req.query.title;
+    let filmDetected = ghibliFilmsJson.filter(ghibliFilm => ghibliFilm.title == titleRequest);
+    res.status(200).send(filmDetected);
 });
